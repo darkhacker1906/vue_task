b2ce60ff99067d4e3c390879be0575a88f7eab3c
darkhacker1906 <singhabhinandan634@gmail.com>
2024-04-05 18:15:06 +0530

    Resolved the queries

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,9 +1,18 @@
 <template>
   <h1>This is the about page</h1>
+  <button @click="show=false">Hide the below comp</button>
+  <user-view v-if="show"/>
 </template>
 
 <script>
+import UserView from '@/components/UserView.vue'
 export default {
+  components: { UserView },
+  data(){
+    return{
+      show:true,
+    }
+  },
 name:'AboutPage'
 }
 </script>
